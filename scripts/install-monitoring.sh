7beac3a158bbf92b6d9118fa3aa71257de3a9d17
purvaa01 <purvaa462@gmail.com>
2026-06-17 08:19:21 +0000

    add k8s monitoring stack with prometheus and grafana

--- scripts/install-monitoring.sh
+++ scripts/install-monitoring.sh
@@ -2,15 +2,22 @@
 
 set -e
 
+echo "Creating monitoring namespace..."
+kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -
+
 echo "Adding Helm repo..."
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
 
 echo "Updating Helm repos..."
 helm repo update
 
-echo "Installing Prometheus + Grafana stack..."
+echo "Installing/Upgrading Prometheus + Grafana..."
 helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
   --namespace monitoring \
-  --create-namespace
+  --create-namespace \
+  --wait
 
 echo "Monitoring stack deployed successfully!"
+
+echo "Current monitoring pods:"
+kubectl get pods -n monitoring
